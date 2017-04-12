1996e57a380518820d552b1519afc2a8311e2e5c
Trevor DeBardeleben <trevordebard@gmail.com>
2017-04-12 18:53:50 -0500

    Fixed the CreateTeamPopUp to look neater

--- app/src/main/java/com/example/logan/test2/CreateTeamPopUp.java
+++ app/src/main/java/com/example/logan/test2/CreateTeamPopUp.java
@@ -21,12 +21,6 @@ public class CreateTeamPopUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_team_pop_up);
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-        getWindow().setLayout((int)(width*.8),(int)(height*.5));
 
         btnCloseTeamPopUp =(Button)findViewById(R.id.cancelTeamCreateButton);
         btnCreateTeamPopUp = (Button)findViewById(R.id.teamCreatePopUpButton);
