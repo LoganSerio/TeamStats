de990ea68b50df4dfe50085c7efb563b8dc4b592
Trevor DeBardeleben <trevordebard@gmail.com>
2017-04-12 19:29:46 -0500

    Created AddPosition page

--- app/src/main/java/com/example/logan/test2/CreatePosition.java
+++ app/src/main/java/com/example/logan/test2/CreatePosition.java
@@ -1,13 +1,24 @@
 package com.example.logan.test2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class CreatePosition extends AppCompatActivity {
+    Button btnCreatePositionAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_position);
+        btnCreatePositionAdd = (Button) findViewById(R.id.CreatePositionAddButton);
+        btnCreatePositionAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CreatePosition.this,AddPosition.class));
+            }
+        });
     }
 }
