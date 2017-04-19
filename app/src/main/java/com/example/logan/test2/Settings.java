b5d866db7567f9a64d3d042e1d3815a6221f14d2
MattLarroquette <mlarroquette4@gmail.com>
2017-04-19 15:52:34 -0500

    Added functionality to edit position button on the team overview tab (brings user to their created positions). Also added user manual button and linked it to the github with the user manual.

--- app/src/main/java/com/example/logan/test2/Settings.java
+++ app/src/main/java/com/example/logan/test2/Settings.java
@@ -1,6 +1,7 @@
 package com.example.logan.test2;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.Button;
 public class Settings extends AppCompatActivity {
     Button btnSendFeedback;
     Button btnUserManual;
+    Button btnTempTeamPage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +18,7 @@ public class Settings extends AppCompatActivity {
 
         btnSendFeedback = (Button) findViewById(R.id.sendFeedbackButton);
         btnUserManual = (Button) findViewById(R.id.reorderTeamsButton);
+        btnTempTeamPage = (Button) findViewById(R.id.btnTempTeamPage);
 
         btnSendFeedback.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +28,19 @@ public class Settings extends AppCompatActivity {
         });
 
         btnUserManual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://github.com/LoganSerio/TeamStats/blob/master/README.md";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        /*
+        THIS IS A TEMPORARY ACCESS TO THE TEAM PAGE, PLEASE DELETE WHEN TEAM PAGE IS LINKED
+         */
+        btnTempTeamPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Settings.this,TeamPage.class));
--- app/src/main/java/com/example/logan/test2/TeamOptions.java
+++ app/src/main/java/com/example/logan/test2/TeamOptions.java
@@ -1,10 +1,14 @@
 package com.example.logan.test2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.Button;
+import android.content.Intent;
 import android.widget.Switch;
 
 /**
@@ -13,9 +17,47 @@ import android.widget.Switch;
 
 public class TeamOptions extends Fragment{
 
+    Button btnEditPositions;
+    Button btnEditStatistics;
+    Button btnEditPlayers;
+    Button btnDeleteTeam;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_team_options, container, false);
+        btnEditPositions = (Button) rootView.findViewById(R.id.editPositionButton);
+        btnEditStatistics = (Button) rootView.findViewById(R.id.editStatsButton);
+        btnEditPlayers = (Button) rootView.findViewById(R.id.editPlayerButton);
+        btnDeleteTeam = (Button) rootView.findViewById(R.id.deleteTeamButton);
+
+        btnEditPositions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TeamOptions.this.getActivity(),TeamPositions.class));
+            }
+        });
+
+        btnEditStatistics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnEditPlayers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnDeleteTeam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         return rootView;
     }
 }
